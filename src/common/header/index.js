f780c7dff6aa701c6821fc467ec7fc7cdc724874
Evan-xia-mew <lovesomuns@gmail.com>
2019-12-07 06:26:33 -0500

    提交文件

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
 import { actionCreators } from './store';
+import { actionCreators as loginActionCreators} from '../../pages/login/store';
 import { Link } from 'react-router-dom';
 import {
   HeaderWrapper,
@@ -61,7 +62,7 @@ getListArea(){
 }
 
   render(){
-    const {focused,handleInputFocus,handleInputBlur,list}=this.props;
+    const {focused,handleInputFocus,handleInputBlur,list,login,logout}=this.props;
     return (
       <HeaderWrapper>
         <Link to='/'>
@@ -70,7 +71,11 @@ getListArea(){
         <Nav>
           <NavItem className='left active'>首页</NavItem>
           <NavItem className='left'>下载App</NavItem>
-          <NavItem className='right'>登录</NavItem>
+          {
+            login ?
+            <NavItem onClick={logout} className='right'>退出</NavItem>:
+            <Link to='/login'><NavItem className='right'>登录</NavItem></Link>
+          }
           <NavItem className='right'>
             <i className="iconfont">&#xe636;</i>
           </NavItem>
@@ -93,8 +98,10 @@ getListArea(){
           </SearchWrapper>
         </Nav>
         <Addition>
-          <Button className='writting'><i className="iconfont">&#xe616;</i>写文章</Button>
-          <Button className='reg'>注册</Button>
+          <Link to='/write'>
+            <Button className='writting'><i className="iconfont">&#xe616;</i>写文章</Button>
+            <Button className='reg'>注册</Button>
+          </Link>
         </Addition>
       </HeaderWrapper>
     );
@@ -109,7 +116,8 @@ const mapStateToProps=(state)=>{
     list: state.getIn(['header','list']),
     page: state.getIn(['header','page']),
     totalPage:state.getIn(['header','totalPage']),
-    mouseIn: state.getIn(['header','mouseIn'])
+    mouseIn: state.getIn(['header','mouseIn']),
+    login:state.getIn(['login','login'])
   }
 }
 
@@ -142,6 +150,9 @@ const mapDispathToProps=(dispatch)=>{
       }else{
         dispatch(actionCreators.changePage(1));
       }
+    },
+    logout(){
+      dispatch(loginActionCreators.logout())
     }
 
   }
--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -13,7 +13,7 @@ class List extends PureComponent{
         {
           list.map((item,index)=>{
             return(
-              <Link key={index} to='/detail'>
+              <Link key={index} to={'/detail/'+item.get('id')}>
               <ListItem>
                 <img alt='' className='list-pic' src={item.get('imgUrl')} />
                 <ListInfo>
